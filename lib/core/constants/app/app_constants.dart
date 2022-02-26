8c9e661556fc0171614c24a52c4ac0b78bdd4577
fatihemree <fatih.emre.212@gmail.com>
2022-02-26 07:10:07 +0300

    sfpro update fix

--- lib/core/constants/app/app_constants.dart
+++ lib/core/constants/app/app_constants.dart
@@ -4,7 +4,7 @@ class AppConstants {
   static const LANG_ASSET_PATH = 'assets/lang';
   static const FONT_FAMILY = 'SfPro';
   static const EMAIL_REGIEX = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}\$';
-  static const FIRBASE_PROJECT_ID = '541990764473';
-  static const FIREBASE_APP_ID =
-      '1:541990764473:android:2f260c4db0f035efb34068';
+  // static const FIRBASE_PROJECT_ID = '541990764473';
+  // static const FIREBASE_APP_ID =
+  //     '1:541990764473:android:2f260c4db0f035efb34068';
 }
