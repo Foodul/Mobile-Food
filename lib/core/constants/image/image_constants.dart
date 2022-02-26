a350f94822b45b7aa9c1ce34ed087ffe19a85616
fatihemree <fatih.emre.212@gmail.com>
2022-02-26 08:56:11 +0300

    splashPage done

--- lib/core/constants/image/image_constants.dart
+++ lib/core/constants/image/image_constants.dart
@@ -8,13 +8,14 @@ class ImageConstants {
   /// example
   /// final example = 'example'.toPNG;
   // final oneriLogo = 'logo'.toPNG;
-  final onBoardBanner = 'onboarding'.toPNG;
-  final successSheetBackground = 'success_sheet_background'.toPNG;
-  final warningBackground = 'warning_sheet_background'.toPNG;
-  final warningSheetIcon = 'warning_sheet_icon'.toPNG;
-  final successSheetIcon = 'success_sheet_icon'.toPNG;
-  final userPointBackground = 'user_point_background'.toPNG;
-  final personImage = 'personImage'.toPNG;
+  // final onBoardBanner = 'onboarding'.toPNG;
+  // final successSheetBackground = 'success_sheet_background'.toPNG;
+  // final warningBackground = 'warning_sheet_background'.toPNG;
+  // final warningSheetIcon = 'warning_sheet_icon'.toPNG;
+  // final successSheetIcon = 'success_sheet_icon'.toPNG;
+  // final userPointBackground = 'user_point_background'.toPNG;
+  // final personImage = 'personImage'.toPNG;
+  final splashBG = 'splashBG'.toPNG;
 }
 
 extension ImageConstantsExtension on String {
